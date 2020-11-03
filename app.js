321750fe30467f73bf824e17a284672112b65205
ANN0Y1NGHACKER <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-03 00:00:40 -0500

    Release 1.0.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
 
-var port = 65300;
+var port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
