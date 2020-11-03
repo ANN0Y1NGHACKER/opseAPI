c40401ba53c1a088e5eadd893415a07f9f9eda60
ANN0Y1NGHACKER <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-03 03:35:32 -0500

    Release 1.0.0

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-require('dotenv-flow').config();
-const bodyParser = require('body-parser');
-const express = require("express");
-const app = express();
-const server = require('http').createServer(app);
+let main = () => {
+	require('dotenv-flow').config();
+	const bodyParser = require('body-parser');
+	const express = require("express");
+	const app = express();
+	const server = require('http').createServer(app);
+	
+	var port = process.env.PORT || 3000;
+	
+	app.use(express.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use(express.static("public"));
+	app.use(require("./routes"));
+	
+	server.listen(port, () => {
+		console.info(`Server listening at port ${port}`);
+	});
+};
 
-var port = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(require("./routes"));
-
-server.listen(port, () => {
-	console.info(`Server listening at port ${port}`);
-});
+setTimeout(main(), 1000);
