4c597a71cff5f8b527ef79708cc7bf31495d71e3
ANN0Y1NGHACKER <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-04 21:13:36 -0500

    Release 1.0.0

--- modules/SQL.js
+++ modules/SQL.js
@@ -1,18 +1,44 @@
 const mysql = require('mysql');
 
-var db = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    port     : process.env.DB_PORT,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_NAME,
-});
-db.connect();
+// var db = mysql.createConnection({
+//     host     : process.env.DB_HOST,
+//     port     : process.env.DB_PORT,
+//     user     : process.env.DB_USER,
+//     password : process.env.DB_PASS,
+//     database : process.env.DB_NAME,
+// });
+// db.connect();
 
-db.on("error", () => {
-	// db.connect();
-	console.log("SQL DISCONNECTED")
-});
+var db;
+
+function connectDatabase() {
+	db = mysql.createConnection({
+		host     : process.env.DB_HOST,
+		port     : process.env.DB_PORT,
+		user     : process.env.DB_USER,
+		password : process.env.DB_PASS,
+		database : process.env.DB_NAME,
+	});
+  
+	db.connect(function(err) {
+	  	if(err) {
+			console.error('Error establishing connection... retrying', err);
+			setTimeout(connectDatabase, 2000);
+		}     
+		else connectionResets = 0;
+	});                            
+
+	db.on('error', function(err) {
+	  	connectionResets++;
+		if (connectionResets < 5) connectDatabase();	                      
+		else {
+			console.error("Could not establish database connection");
+			throw err;     
+		}
+	});
+}
+
+connectDatabase();
 
 exports.getTeams = async () => {
     return new Promise((res, err) => {
