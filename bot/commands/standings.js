812a1d7fbe986ad20b50ee3aab81568ec6084b2d
Taimoor Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-20 22:07:28 -0500

    Update 1.0.1

--- bot/commands/standings.js
+++ bot/commands/standings.js
@@ -1,7 +1,23 @@
 const request = require('request-promise');
 
-let sendEmbed = (channel, data) => {
+let sendEmbed = (channel, data, filtered=false) => {
+    let embed = {
+        "title": "",
+        "description": "",
+        "color": 14100527,
+        "timestamp": new Date(),
+        "thumbnail": {
+            "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        }
+    }
 
+    if (filtered) for (var i in data) {
+        n = parseInt(i)+1;
+        if (n<10) n = `0${n}`
+        embed.description += `\`${n}\` - **${data[i].wins}W ${data[i].loss}L** - ${data[i].name}\n`;
+    }
+
+    channel.send({ embed });
 }
 
 exports.run = (client, message, args) => {
@@ -11,8 +27,11 @@ exports.run = (client, message, args) => {
 
     if (args[0]) {
         switch (args[0]) {
-            case value:
-                
+            case "1":
+            case "lol":
+                request("http://api.opsesports.ca/standings/1").then(body => {
+                    sendEmbed(message.channel, JSON.parse(body), true)
+                });
                 break;
         
             default:
