fd6f2265ade67767b0c0037cc2228843248ca45e
Taimoor Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-23 15:39:28 -0500

    Update 1.0.2

--- modules/MatchNotifier.js
+++ modules/MatchNotifier.js
@@ -14,29 +14,18 @@ schedule.scheduleJob('00 * * * * *', async () => {
 
     for (var i in games) {
         let gTime = new Date(games[i].date);
-        // gTime.setHours(gTime.getHours() + 2);
-        // gTime.setMinutes(gTime.getMinutes() - 1);
+        // gTime.setHours(gTime.getHours() - 2);
+        gTime.setMinutes(gTime.getMinutes() - 2);
 
-        // if (
-        //     gTime.getFullYear() == cTime.getFullYear() &&
-        //     gTime.getMonth() == cTime.getMonth() &&
-        //     gTime.getDate() == cTime.getDate() &&
-        //     gTime.getHours() == cTime.getHours() &&
-        //     gTime.getMinutes() == cTime.getMinutes() + 1
-        // ) gamesToNotify.push(games[i]);
-
-        // console.log(`${gTime.getTime()} - ${cTime.getTime()}`)
-        console.log(`${gTime.toString()} - ${cTime.toString()}`)
-
-        // console.log(gTime.getTime() == cTime.getTime())
+        if (gTime == cTime) gamesToNotify.push(games[i]);
     }
 
     for (var i in gamesToNotify) {
         let team1 = teamChannels.filter(t => t.id == gamesToNotify[i].teamID1)[0];
         let team2 = teamChannels.filter(t => t.id == gamesToNotify[i].teamID2)[0];
 
-        discord.sendMessage(team1.channel, `You have a game against **${team2.name}** in 1 minute.`);
-        discord.sendMessage(team2.channel, `You have a game against **${team1.name}** in 1 minute.`);
+        discord.sendMessage(team1.channel, `You have a game against **${team2.name}** in 2 minutes.`);
+        discord.sendMessage(team2.channel, `You have a game against **${team1.name}** in 2 minutes.`);
     }
 
     if (gamesToNotify.length > 0) console.log(`[MATCH NOTIFIER] Notified ${gamesToNotify.length} games.`);
