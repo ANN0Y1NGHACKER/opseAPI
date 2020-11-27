47b537b537fa8646f99570fee73885589b705c0c
Taimoor Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-26 21:33:41 -0500

    Update 1.0.2

--- routes.js
+++ routes.js
@@ -241,7 +241,7 @@ router.post('/lolMatchResult', async (req, res) => {
 
 	let isOver = await API.saveGame(body);
 	let date = new Date();
-	date.setTime(date.getTime()-1.8e7);
+	date.setTime(date.getTime()-18000);
 	if (!isOver[0]) {
 		axios.post(`https://discord.com/api/webhooks/${config.WEBHOOK_ID}/${config.WEBHOOK_TOKEN}`, {
 			"content": `**[Click here](http://api.opsesports.ca/createImg-wide?game=lol&l_score=${isOver[1][body.metaData.team1_ID]}&r_score=${isOver[1][body.metaData.team2_ID]}&line1=LIVE%20NOW&line2=Regular%20Season&line3=${new Intl.DateTimeFormat('en', { month: 'short' }).format(date)}%20${date.getDate()},%20${date.getFullYear()}&left=${teamChannels.filter(t => t.id == body.metaData.team1_ID)[0].imgID}&right=${teamChannels.filter(t => t.id == body.metaData.team2_ID)[0].imgID}&download=true)** to download`
