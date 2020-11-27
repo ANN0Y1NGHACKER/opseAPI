a4c2048613b4beb29813961542e287bc970d3410
Taimoor Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2020-11-27 11:39:24 -0500

    Update 1.0.3

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -5,13 +5,13 @@ module.exports = async client => {
 	console.log(` `);
 	console.log(`Logged in as ${client.user.tag}!`);
 
-	client.user.setPresence({
-        // status: "invisible",
-        // game: {
-        //     name: "ANN0Y1NGHACKER code me",
-        //     type: "WATCHING" // PLAYING, WATCHING, LISTENING, STREAMING,
-        // }
-    });
+	// client.user.setPresence({
+    //     // status: "invisible",
+    //     // game: {
+    //     //     name: "ANN0Y1NGHACKER code me",
+    //     //     type: "WATCHING" // PLAYING, WATCHING, LISTENING, STREAMING,
+    //     // }
+    // });
 
 	var rMa = [
 		// ["649150418106056704", "649158504762048532"]
