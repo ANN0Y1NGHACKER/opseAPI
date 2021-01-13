fa0929fc8810aea861452aa99c5f6e9a37968caa
Taimoor-Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2021-01-13 11:25:27 -0500

    DELETE ALL

--- bot/events/messageReactionAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client, msgReaction, user) => {
-	var emoji = msgReaction.emoji;
-	// var member = msgReaction.message.guild.members.find(member => member.id === user.id);
-
-	if (user.bot) return;
-
-};
