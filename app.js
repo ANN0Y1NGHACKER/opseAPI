cf50504db742829f94d8f0966a250ddcce747188
Taimoor-Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2021-01-18 22:02:52 -0500

    Start of admin dashboard

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ require('dotenv-flow').config();
 const config = require('./config');
 
 const
-    bodyParser = require('body-parser'),
     express = require('express'),
     app = express()
 
-app.use(bodyParser.json())
+app.use(require('express-session')({
+        secret: process.env.SESSION_KEY,
+        resave: false,
+        saveUninitialized: true
+    }))
     .use(express.urlencoded({ extended: true }))
+    .use(require('body-parser').json())
     .set('json replacer', null)
     .set('json spaces', 4)
     .use(require('./routes'))
+    .use('/admin', require('./modules/admin'))
     .use('/image-generator', require('./modules/image-generator'))
     .use(express.static('public'))
 
---
+++ modules/admin/index.js
@@ -0,0 +1,43 @@
+const
+    express = require('express'),
+    router = express.Router(),
+    sass = require('sass'),
+    pug = require('pug')
+
+let checkSession = (req, res, next) => {
+    if (!req.session || !req.session.user) {
+        let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        req.session.oldURL = fullUrl;
+        res.redirect('/login');
+    } else next();
+}
+
+router.get('/', async (req, res) => {
+    if (!req.session || !req.session.user) res.redirect('/admin/login');
+    else res.redirect('/admin/dashboard');
+});
+
+router.get('/dashboard', checkSession, async (req, res) => {
+    res.send(pug.renderFile(`${__dirname}/views/dashboard.pug`, {
+        style: sass.renderSync({file: `${__dirname}/sass/dashboard.scss`}).css.toString(),
+    }));
+});
+
+router.route('/login')
+    .get(async (req, res) => {
+        res.send(pug.renderFile(`${__dirname}/views/login.pug`, {
+            style: sass.renderSync({file: `${__dirname}/sass/login.scss`}).css.toString(),
+        }));
+    })
+    .post(async (req, res) => {
+        if (!req.body.password) res.sendStatus(403);
+        else {
+            if (req.body.password === process.env.API_PASSWORD) {
+                req.session.user = { username: "admin" };
+                res.redirect('/admin');
+            }
+            else res.redirect('/admin');
+        }
+    })
+
+module.exports = router;
